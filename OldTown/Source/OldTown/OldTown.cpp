81dba1ace78ec643c85258e51ef427feff6c1e85
warner <wscheibe@ucsc.edu>
2020-08-18 09:47:10 -0700

    added project here

---
+++ OldTown/Source/OldTown/OldTown.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "OldTown.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OldTown, "OldTown" );
